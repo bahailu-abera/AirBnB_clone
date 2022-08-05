299980c55d12d25d17aa03119fc06c65d0135cfc
bahailu <aberabahailu4@gmail.com>
2022-08-05 23:51:08 +0300

    state

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module for State data """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Class State that inherities from BaseModel """
+    name = ""
