5f1f4e310cdaec6a9e92d440da2e17c5835a29d2
bahailu <aberabahailu4@gmail.com>
2022-08-05 23:49:41 +0300

    amenity

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module for Amenity data """
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Class Amenity that inherities from BaseModel """
+    name = ""
