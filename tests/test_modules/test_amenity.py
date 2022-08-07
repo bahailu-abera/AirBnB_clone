c344729c3c86a1f3b418d199507c4694c33acb12
bahailu <aberabahailu4@gmail.com>
2022-08-07 14:25:37 +0300

    test amenity

---
+++ tests/test_modules/test_amenity.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" Module for testing BaseModel functionality and Documentation  """
+from models import amenity
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models import storage
+import unittest
+import os
+
+
+class TestAmenityMethods(unittest.TestCase):
+    """TestAmenityClass test for the inheretit class
+    Amenity, this tests that the output is as expected
+    Args:
+        unittest (): Propertys for unit testing
+    """
+
+    def setUp(self):
+        """ condition to test file saving """
+        with open("test.json", 'w'):
+            storage._FileStorage__file_path = "test.json"
+            storage._FileStorage__objects = {}
+        Amenity.name = ""
+
+    def tearDown(self):
+        """ destroys created file """
+        storage._FileStorage__file_path = "file.json"
+        try:
+            os.remove("test.json")
+        except FileNotFoundError:
+            pass
+
+    def test_module_doc(self):
+        """ check for module documentation """
+        self.assertTrue(len(amenity.__doc__) > 0)
+
+    def test_class_doc(self):
+        """ check for class documentation """
+        self.assertTrue(len(Amenity.__doc__) > 0)
+
+    def test_method_docs(self):
+        """ check for method documentation """
+        for func in dir(Amenity):
+            self.assertTrue(len(func.__doc__) > 0)
+
+    def test_isintance(self):
+        """ check if object is basemodel instance """
+        obj = Amenity()
+        self.assertIsInstance(obj, BaseModel)
+        self.assertIsInstance(obj, Amenity)
+
+    def test_field_types(self):
+        """ test field attributes of user """
+        my_amenity = Amenity()
+        self.assertTrue(type(my_amenity.name) == str)
+
+
+if __name__ == "__main__":
+    unittest.main()
